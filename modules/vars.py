567e5883f1b67d3556e79e2f00e794f5db897bc6
eaglebosshere <165704787+eaglebosshere@users.noreply.github.com>
2024-04-04 09:34:11 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "24210931"
-api_hash = "ac5b43fbcf0302a87d2c3106c614c712"
-bot_token = "6474981890:AAEKbALoK2XJyca6oXgW66T3ftIujlk2Ysw"
+api_id = "23989445"
+api_hash = "f9d8341dc5ad6978e623f34df457b6ed"
+bot_token = "6700172868:AAFf-yHdF9JHEmVW9yNBKRkzeik1X2ijZok"
